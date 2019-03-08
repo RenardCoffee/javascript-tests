e3bf505b569d7514e57587f1ab6b74392bfdc265
unknown <blac3fox@hotmail.com>
2019-03-08 10:37:45 -0800

    Newfilereader

--- regexp.php
+++ regexp.php
@@ -3,23 +3,25 @@
 <body>
 
 <p>Click the button to diferenciate from a Variable and a Number!!</p>
-<input id="textbox" type="text" />
-
-<textarea rows="15" cols="50" id="myTextarea">
-</textarea>
-
-<input type="file" name="file" id="file">
+  <?php
+$filename = 'regexp.txt';
+$eachlines = file($filename, FILE_IGNORE_NEW_LINES);//create an array
+echo '<select name="value" id="value">';
+foreach($eachlines as $lines){
+    echo "<option>{$lines}</option>";
+}
+echo '</select>';
+  ?>
 <button onclick="isVariable()">What iam?</button>
 
 <p id="res"></p>
-
 <script>
 document.getElementById('file').onchange = function(){
   var file = this.files[0];
   var reader = new FileReader();
   reader.onload = function(progressEvent){
     // Entire file
-    document.getElementById("myTextarea").value = this.result;
+    //document.getElementById("myTextarea").value = this.result;
     //console.log(this.result);
     // By lines
     var lines = this.result.split('\n');
@@ -32,11 +34,9 @@ document.getElementById('file').onchange = function(){
 };
 
 function isVariable() {
-  var str = document.getElementById("myTextarea").value;
-  var lines = srt.split('\n');
-  for(var line = 0; line < lines.length; line++){
-    var str = lines[line];
-//  var str = document.getElementById("myTextarea").value;
+  var str = document.getElementById("value").value;
+  //var strUser = e.options[e.selectedIndex].value;
+  //var str = document.getElementById("myTextarea").value;
   var patt1 = /^[0-9]+$/;
   var patt2 = /^[a-zA-Z]+([a-zA-Z]+|[0-9]+)*$/;
   var patt3 = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
@@ -57,9 +57,8 @@ function isVariable() {
   {
   	var result = "Not valid";
   }
-  //document.getElementById("res").innerHTML = result;
+  document.getElementById("res").innerHTML = result;
   console.log(result);
-}
 }
 </script>
 
